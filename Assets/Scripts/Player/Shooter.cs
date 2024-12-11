8881dcce8f46a32b58acb5101e290f4da4a90e73
Ahmed ElShenawy <elshenawy.ahmed@gmail.com>
2024-12-11 11:42:58 +0200

    Add shooting mechanic

--- Assets/Scripts/Player/Shooter.cs
+++ Assets/Scripts/Player/Shooter.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
+    // Reference to the original prefab to instatiate
+    public GameObject projectilePrefab;
+    
+	// Reference to the AudioSource component on the player
+	public AudioSource sfxPlayer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,17 @@ public class Shooter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetButtonDown("Jump"))
+        {
+            Shoot();
+        }
+    }
 
+    void Shoot()
+    {	
+		// We instantiate the prefab at the same position as the player,
+        // since this script is on the player GameObject
+        Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+        sfxPlayer.Play();
     }
 }
