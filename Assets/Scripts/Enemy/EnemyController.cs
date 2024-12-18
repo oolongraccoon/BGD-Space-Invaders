ba394be3f6fe2a3b3827f6975d5bdf54a768549c
oolongraccoon <133898805+oolongraccoon@users.noreply.github.com>
2024-12-18 02:42:17 +0200

    changed graphica and sounds

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
@@ -7,74 +8,70 @@ public class EnemyController : MonoBehaviour
     public float maxPosX;
 
     public float moveDistance = 1f;
-
-    bool isMovingRight = true;
-
-
     public float timeStep = 1f;
     public float countdown;
-	
-	// I added a switch to try both methods
-	public bool isUsingCountdown = true;
 
-    // Use this for initialization
+    private bool isMovingRight = true;
+    public bool isUsingCountdown = true;
+
     void Start()
     {
-		if (isUsingCountdown)
-		{
-			countdown = timeStep;
-		}
-		else
-		{	
-			// Invoke repeating will be called once after timeStep (2nd parameter) amount,
-			// and then repeatedly every timeStep (3rd parameter) amount
-			//InvokeRepeating("Move", timeStep, timeStep);
-			InvokeRepeating("Move", timeStep, timeStep);
-		}
+        if (isUsingCountdown)
+        {
+            countdown = timeStep;
+        }
+        else
+        {
+            InvokeRepeating("Move", timeStep, timeStep);
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-		if (isUsingCountdown)
-		{
-			countdown -= Time.deltaTime;
+        if (isUsingCountdown)
+        {
+            countdown -= Time.deltaTime;
 
-			if (countdown <= 0)
-			{
-				Move();
-				countdown = timeStep;
-			}
-		}
+            if (countdown <= 0)
+            {
+                Move();
+                countdown = timeStep;
+            }
+        }
     }
 
     void Move()
     {
         if (isMovingRight)
         {
-            // Moving right
             Vector3 currentPos = transform.position;
-            Vector3 newPos = currentPos + new Vector3(moveDistance, 0f);
+            Vector3 newPos = currentPos + new Vector3(moveDistance, 0f, 0f);
             transform.position = newPos;
 
-            // If aliens group reached the right-most edge, flip their direction
             if (transform.position.x >= maxPosX)
             {
                 isMovingRight = false;
+                MoveDown();
             }
         }
         else
         {
-            // Moving left
             Vector3 currentPos = transform.position;
-            Vector3 newPos = currentPos - new Vector3(moveDistance, 0f);
+            Vector3 newPos = currentPos - new Vector3(moveDistance, 0f, 0f);
             transform.position = newPos;
 
-            // If aliens group reached the left-most edge, flip their direction
             if (transform.position.x <= minPosX)
             {
                 isMovingRight = true;
+                MoveDown();
             }
         }
+    }
+
+    void MoveDown()
+    {
+        Vector3 currentPos = transform.position;
+        Vector3 newPos = currentPos - new Vector3(0f, moveDistance, 0f);
+        transform.position = newPos;
     }
 }
